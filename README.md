effee2b398bca5772a46ce034a9a9c3922e3f9c2
ijimmoore <ijimmoore@gmail.com>
2020-07-12 03:18:56 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # nuxt-cms
-nust plus a cms demo
+This is phase 1 of a project to build a Nuxtjs and CMS demo
+
+All code for this Nuxt project was either cloned from 
+https://github.com/nuxt-company/demo-blog-nuxt-content 
+.... or entered following this tutorial: 
+https://nuxtjs.org/blog/creating-blog-with-nuxt-content
+imo, the tutorial has some hiccups, but you may get by looking at the clone and the Nuxt and Tailwind docs
+
+## Blog Build Setup
+
+```bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
+```
+
+For a detailed explanation on how Nuxtjs works, check out [Nuxt.js docs](https://nuxtjs.org).
+
---
+++ components/TheHeader.vue
@@ -0,0 +1,52 @@
+<template>
+  <header class="flex">
+    <nav class="main-nav">
+        <ul class="nav-links">
+            <NuxtLink to="/" tag="li"><a>All Posts</a></NuxtLink>
+            <NuxtLink to="/about" tag="li"><a>About</a></NuxtLink>
+        </ul>
+    </nav>
+    <NuxtLink to="/"><Logo class="mb-4" /></NuxtLink>
+    <AppSearchInput class="ml-8" />
+  </header>
+</template>
+
+<style scoped>
+.main-header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    background: #022d30;
+    height: 4.5rem;
+}
+
+.main-nav {
+    height: 100%
+}
+
+.nav-links {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+}
+
+.nav-link {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 rem;
+    padding: 0.3rem;
+}
+
+.nav-link a {
+    display: block;
+    text-decoration: none;
+
+}
+</style>
